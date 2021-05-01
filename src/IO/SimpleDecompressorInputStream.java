01ccb0561c44346df210275c34f05d57b5f972ad
hodisan94 <dhodisan@gmail.com>
2021-05-01 18:45:31 +0300

    Part B

--- src/IO/SimpleDecompressorInputStream.java
+++ src/IO/SimpleDecompressorInputStream.java
@@ -16,4 +16,25 @@ public class SimpleDecompressorInputStream  extends InputStream {
     public int read() throws IOException {
         return 0;
     }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        int counter = 0;
+        int read = in.read();
+
+        while (read != -1){
+            if (counter < 12){
+                b[counter] = 0x00;
+                counter++;
+                b[counter] = 0x00;
+
+
+            }
+        }
+
+
+
+
+        return super.read(b);
+    }
 }
