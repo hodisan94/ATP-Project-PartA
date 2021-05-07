5a224aa631c073da1e57cd2f99a56b6db213787a
hodisan94 <dhodisan@gmail.com>
2021-05-07 19:31:18 +0300

    Part B

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -1,4 +1,29 @@
 package Server;
 
+import algorithms.search.ASearchingAlgorithm;
+import algorithms.search.BreadthFirstSearch;
+
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Configurations {
+
+    private static Properties properties = new Properties();
+    private static int thread_num = 1; //defult;
+    private static ASearchingAlgorithm algorithm = new BreadthFirstSearch();//defult
+    private static InputStream input = null;
+
+    private static Configurations single_instance = null;
+
+
+    public static Configurations getInstance(){
+        if (single_instance == null){
+            single_instance = new Configurations();
+        }
+        return single_instance;
+    }
+
+
+
+
 }
