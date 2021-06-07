f0fe07a511ba8fda529b660512f3d9526cac35d2
hodisan94 <dhodisan@gmail.com>
2021-06-07 14:43:43 +0300

    Part B

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package Server;
 
 import algorithms.mazeGenerators.AMazeGenerator;
@@ -12,75 +17,53 @@ import java.io.*;
 import java.util.Properties;
 
 public class Configurations {
-
     private static Properties properties = new Properties();
-    private static int threadPoolSize = 1; //defult;
+    private static int threadPoolSize = 1;
     private static String mazeGeneratingAlgorithm;
     private static String mazeSearchingAlgorithm;
     private static InputStream input = null;
 
     private static Configurations single_instance = null;
 
-    private Configurations(){
+    private Configurations() {
         try {
             String filename = "resources\\config.properties";
             //input = new FileInputStream("C:\\ATP-Project-PartA\\resources\\config.properties");
             input = new FileInputStream(filename);
             properties.load(input);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException var2) {
+            var2.printStackTrace();
+        } catch (IOException var3) {
+            var3.printStackTrace();
         }
     }
 
-
-    /**
-     * @return its a singelton class so if the object exists already we will return him.
-     */
-    public static Configurations getInstance(){
-        if (single_instance == null){
+    public static Configurations getInstance() {
+        if (single_instance == null) {
             single_instance = new Configurations();
         }
         return single_instance;
     }
 
-    /**
-     * @return the type of searching algorithm
-     */
-    public static ASearchingAlgorithm getMazeSearchingAlgorithm(){
+    public static ASearchingAlgorithm getMazeSearchingAlgorithm() {
         String maze_test = properties.getProperty("mazeGeneratingAlgorithm");
         String maze_search_name = properties.getProperty("mazeSearchingAlgorithm");
-        if (maze_search_name.equals("BreadthFirstSearch")){
+        if (maze_search_name.equals("BreadthFirstSearch")) {
             return new BreadthFirstSearch();
+        } else {
+            return (ASearchingAlgorithm)(maze_search_name.equals("DepthFirstSearch") ? new DepthFirstSearch() : new BestFirstSearch());
         }
-        else if (maze_search_name.equals("DepthFirstSearch")){
-            return new DepthFirstSearch();
-        }
-        else
-            return new BestFirstSearch(); // defult
     }
 
-    /**
-     * @return the maze algorithm that we will use
-     */
     public static AMazeGenerator getMazeGeneratingAlgorithm() {
         String maze_generate_name = properties.getProperty("mazeGeneratingAlgorithm");
-        if (maze_generate_name.equals("SimpleMazeGenerator")){
-            return new SimpleMazeGenerator();
-        }
-        else
-        return new MyMazeGenerator();
+        return (AMazeGenerator)(maze_generate_name.equals("SimpleMazeGenerator") ? new SimpleMazeGenerator() : new MyMazeGenerator());
     }
 
     public static int getThreadPoolSize() {
         String thread_size = properties.getProperty("threadPoolSize");
         int size = Integer.valueOf(thread_size);
-        if (size > 0 ){
-            return size;
-        }
-        else
-        return 1;//defult size of 1 thread.
+        return size > 0 ? size : 1;
     }
     public static void setProp(int size, String mazeGeneratingAlgorithm, String mazeSearchingAlgorithm) {
         OutputStream output = null;
@@ -103,6 +86,4 @@ public class Configurations {
             }
         }
     }
-
-
 }
